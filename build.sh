c51745ea2de501ba4d55242986ce157ce893ee19
ikapper <ikapdev@gmail.com>
2019-03-19 13:10:52 +0900

    Jekylled

--- build.sh
+++
@@ -1,9 +0,0 @@
-# https://github.com/envygeeks/jekyll-docker/blob/master/README.md
-
-# jekyllをdockerでやろうとしたが、まだ検討中
-
-export JEKYLL_VERSION=3.8
-docker run --rm \
-  --volume="$PWD:/srv/jekyll" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll build
